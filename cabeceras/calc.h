dc3f9bcc59bfb0c36b2b1f4d529cadd7c9340262
Medichi2002 <pablo.medina108@alu.ulpgc.es>
2024-03-08 13:48:54 +0000

    Refactor

---
+++ cabeceras/calc.h
@@ -0,0 +1,6 @@
+//cabecera calc.h
+int suma(int,int);
+int resta(int,int);
+int producto(int,int);
+float division(float,float);
+
---
+++ cabeceras/mistring.h
@@ -0,0 +1,6 @@
+//cabecera de mistring.h
+int mi_strlen(char* str);
+char* mi_strcpy(char* s1, char* s2);
+char* mi_strcat(char* s1, char* s2);
+char* mi_strdup(char* str);
+char* mi_strequals(char* s1, char* s2);
