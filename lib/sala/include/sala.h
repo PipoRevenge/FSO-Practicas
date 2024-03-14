3f5080d5b13f81a474ea7480e772effc9fad2c16
Medichi2002 <pablo.medina108@alu.ulpgc.es>
2024-03-14 19:27:19 +0000

    Modification of the header sala.h

--- lib/sala/include/sala.h
+++ lib/sala/include/sala.h
@@ -1,9 +1,10 @@
 //Cabecera sala.h
 
 /*
-   Función: Crea la sala y establece todos los asientos libres 
+   Función: Crea una sala y establece todos los asientos libres 
    Parámetros: La capacidad que debe soportar la sala
-   Return: La capacidad de la sala si ha ido todo bien. -1 si hay algún error.
+   Return: La capacidad de la sala si ha ido todo bien. 
+   	  Devuelve -1 si hay algún error.
 */
 	int crea_sala(int capacidad);
 
@@ -11,58 +12,68 @@
 /*
    Función: Elimina la sala
    Parámetros: No
-   Return: 0 si ha ido todo bien. -1 si hay algún error.
+   Return: 0 si ha ido todo bien.
+   	  Devuelve -1 si hay algún error.
 */
 	int elimina_sala();
 
 
 /*
-   Función: Reserva asiento en la sala
-   Parámetros: Id para
-   Return: 0 si ha ido todo bien. -1 si hay algún error.
+   Función: Reserva el primer asiento libre disponible en la sala
+   Parámetros: Id a almacenar en la posición de memoria (Debe ser entero positivo)
+   Return: Devuelve el número de asiento si se ha podido reservar. 
+      	  Devuelve -1 si hay algún error.
 */
 	int reserva_asiento(int id_persona);
 
 
 /*
-   Función: Elimina la sala
-   Parámetros: No
-   Return: 0 si ha ido todo bien. -1 si hay algún error.
+   Función: Libera un asiento de la sala
+   Parámetros: Id de asiento que se va a liberar (rango: 1-capacidad)
+   Return: Si se ha podido liberar, devuelve el id de la persona que ocupaba el asiento. 
+   	  Devuelve -1 si hay algún error.
 */
 
 	int libera_asiento(int id_asiento);
 
 	
 /*
-   Función: Elimina la sala
-   Parámetros: No
-   Return: 0 si ha ido todo bien. -1 si hay algún error.
+   Función: Muestra el estado de un asiento
+   Parámetros: Id del asiento para comprobar si está ocupado o libre (rango: 1-capacidad)
+   Return: Devuelve el id de la persona si el asiento está ocupado. 
+   	  Devuelve 0 si está libre. 
+   	  Devuelve -1 si hay algún error.
 */
 
 	int estado_asiento(int id_asiento);
 
 
 /*
-   Función: Elimina la sala
+   Función: Muestra el número de asientos libres
    Parámetros: No
-   Return: 0 si ha ido todo bien. -1 si hay algún error.
+   Return: Devuelve el número de asientos libres que hay en la sala.
+           Devuelve -1 si hay algún error.
+   
 */
 
 	int asientos_libres();
 	
 /*
-   Función: Elimina la sala
+   Función: Muestra el número de asientos ocupados
    Parámetros: No
-   Return: 0 si ha ido todo bien. -1 si hay algún error.
+   Return: Devuelve el número de asientos ocupados que hay en la sala.
+           Devuelve -1 si hay algún error.
+   
 */
 	
 	int asientos_ocupados();
 	
 
 /*
-   Función: Elimina la sala
+   Función: Muestra la capacidad de la sala
    Parámetros: No
-   Return: 0 si ha ido todo bien. -1 si hay algún error.
+   Return: Devuelve la capacidad de la sala.
+           Devuelve -1 si hay algún error.
 */
 
 	int capacidad_sala();
