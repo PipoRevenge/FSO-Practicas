5b9e7e692847353eb42653bec6296c4bce373133
PipoRevenge <95238885+PipoRevenge@users.noreply.github.com>
2024-03-15 17:31:18 +0000

    Delete cabeceras directory

--- cabeceras/mistring.h
+++
@@ -1,6 +0,0 @@
-//cabecera de mistring.h
-int mi_strlen(char* str);
-char* mi_strcpy(char* s1, char* s2);
-char* mi_strcat(char* s1, char* s2);
-char* mi_strdup(char* str);
-char* mi_strequals(char* s1, char* s2);
